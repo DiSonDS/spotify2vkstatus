1ad507207c873485f8327feb071fb5eb7be8f05b
DiSonDS <dison.ds@gmail.com>
2018-10-14 20:55:00 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,12 @@
+# Spotify
+SPOTIFY_CLIENT_ID = ''
+SPOTIFY_CLIENT_SECRET = ''
+SPOTIFY_REDIRECT_URL = 'http://localhost:8888/callback/'
+SPOTIFY_USERNAME = ''
+SPOTIFY_SCOPE = 'user-read-currently-playing'
+
+# VK.com
+VK_USER_TOKEN = ''
+
+# General
+INTERVAL = 30
